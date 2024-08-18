348bf6799ba6401aa6249a7f25103341017e702d
Agyeiwaa00 <scoogwinkworth@gmail.com>
2024-08-18 18:12:10 +0000

    update

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -9,8 +9,8 @@ to the platform so you can provide the most accurate and up-to-date information 
 // POST function to handle incoming requests
 export async function POST(req) {
   const openai = new OpenAI({
-    apiKey:
-      ''
+    //apiKey:
+      
   });
 
   const data = await req.json();
